43c3deb485558cd778b3f6dfed781f07568f2efb
AutJ <autumnal_joy@foxmail.com>
2024-09-28 00:09:11 +0800

    3295. 举报垃圾信息

---
+++ 3295/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  bool reportSpam(const vector<string> &message,
+                  const vector<string> &bannedWords) {
+    auto blacklist =
+        unordered_set<string>(bannedWords.begin(), bannedWords.end());
+    int cnt = 0;
+    for (const auto &word : message) {
+      if (blacklist.count(word)) {
+        cnt++;
+      }
+      if (cnt > 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+int main() {
+  auto s = Solution();
+  cout << s.reportSpam({"hello", "world", "leetcode"}, {"world", "hello"})
+       << endl;
+  return 0;
+}

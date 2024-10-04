f9ba70cfe5c6e12162a328dbdbd624e2063e80da
AutJ <autumnal_joy@foxmail.com>
2024-10-04 22:51:55 +0800

    554. 砖墙

---
+++ 554/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int leastBricks(vector<vector<int>> &wall) {
+    auto crack = unordered_map<int, int>();
+    for (const auto &i : wall) {
+      int loc = 0;
+      for (int j = 0; j < i.size() - 1; ++j) {
+        loc += i[j];
+        ++crack[loc];
+      }
+    }
+    int maxCrack = 0;
+    for (const auto &p : crack) {
+      maxCrack = max(maxCrack, p.second);
+    }
+    return static_cast<int>(wall.size()) - maxCrack;
+  }
+};
+
+int main() {
+  auto s = Solution();
+  return 0;
+}

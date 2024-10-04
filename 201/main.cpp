af9246fc17e4b0c3045ecb40e12d5c464ab19b07
AutJ <autumnal_joy@foxmail.com>
2024-10-05 02:41:20 +0800

    201. 数字范围按位与

---
+++ 201/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 1073741824 ~ 2147483647
+class Solution {
+public:
+  int rangeBitwiseAnd(int left, int right) {
+    int res = -1;
+    while (left <= right && left > 0) {
+      res &= left;
+      left = static_cast<int>(1ll + left + (left & -left));
+    }
+    return res;
+  }
+};
+
+int main() {
+  auto s = Solution();
+  cout << s.rangeBitwiseAnd(5, 7) << endl;
+  return 0;
+}

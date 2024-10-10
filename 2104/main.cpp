2b2ab7c0faab1cbf503b717c4982a42aea172217
AutJ <autumnal_joy@foxmail.com>
2024-10-11 01:26:56 +0800

    todo: 2104 单调栈

---
+++ 2104/main.cpp
@@ -0,0 +1,19 @@
+#include <limits>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+
+// 1 5 5 5 2 5 5 1
+class Solution {
+public:
+    long long subArrayRanges(vector<int> &nums) {
+
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}

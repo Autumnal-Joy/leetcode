2a4f8f17d370a96a3e8d520f83dd26674e937c5e
AutJ <autumnal_joy@foxmail.com>
2024-10-11 10:11:44 +0800

    面试题 17.20. 连续中值

---
+++ Interview17.20/main.cpp
@@ -0,0 +1,51 @@
+#include <set>
+
+using namespace std;
+
+class MedianFinder {
+    multiset<int> nums;
+    multiset<int>::iterator left, right;
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {}
+
+    // O(logn) 红黑树
+    void addNum(int num) {
+        nums.insert(num);
+        if (nums.size() == 1) {
+            left = right = nums.begin();
+        } else {
+            if (nums.size() & 1) {
+                if (num < *left) {
+                    right--;
+                } else if (num >= *right) {
+                    left++;
+                } else {
+                    left++;
+                    right = left;
+                }
+            } else {
+                if (num < *left) {
+                    left--;
+                } else {
+                    right++;
+                }
+            }
+        }
+    }
+
+    double findMedian() {
+        return (*left + *right) / 2.0;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+
+int main() {
+    return 0;
+}
---
+++ Interview17.20/main2.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class MedianFinder {
+    vector<int> nums;
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {}
+
+    // O(n) 有序数组
+    void addNum(int num) {
+        auto it = std::upper_bound(nums.begin(), nums.end(), num);
+        nums.insert(it, num);
+    }
+
+    double findMedian() {
+        if (nums.size() & 1) {
+            return nums[nums.size() / 2];
+        } else {
+            return (nums[nums.size() / 2] + nums[nums.size() / 2 - 1]) / 2.0;
+        }
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+
+int main() {
+    return 0;
+}

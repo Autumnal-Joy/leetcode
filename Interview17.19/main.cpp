9c6974a60a1b1f6bfc8cdb00ceec3f5ce3b80f44
AutJ <autumnal_joy@foxmail.com>
2024-10-11 11:41:44 +0800

    面试题 17.19. 消失的两个数字

---
+++ Interview17.19/main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> missingTwo(vector<int> &nums) {
+        auto n = static_cast<long long>(nums.size()) + 2;
+        auto a = (1 + n) * n / 2 - accumulate(nums.cbegin(), nums.cend(), 0ll);
+        auto b = (1 + n) * n / 2 * (2 * n + 1) / 3 - inner_product(nums.cbegin(), nums.cend(), nums.cbegin(), 0ll);
+        auto diff = static_cast<long long>(sqrt(2 * b - a * a));
+        return {static_cast<int>((a + diff) / 2), static_cast<int>((a - diff) / 2)};
+    }
+};
+
+int main() {
+    auto s = Solution();
+    auto v = vector<int>({5, 6, 7, 8, 9, 1, 3, 4});
+    s.missingTwo(v);
+    return 0;
+}
---
+++ Interview17.19/main2.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int> missingTwo(vector<int> &nums) {
+        int n = static_cast<int>(nums.size()) + 2;
+
+        int sum = n * (n + 1) / 2;
+        for (auto num: nums) {
+            sum -= num;
+        }
+
+        double product = 0;
+        for (int i = 0; i < n; ++i) {
+            product += std::log(i + 1);
+        }
+        for (auto num: nums) {
+            product -= std::log(num);
+        }
+        product = std::exp(product);
+        double delta = sqrt(sum * sum - 4 * product);
+        double a = (sum + delta) / 2;
+        double b = sum - a;
+
+        return {static_cast<int>(std::round(a)), static_cast<int>(std::round(b))};
+    }
+};
+
+int main() {
+    auto s = Solution();
+    auto v = vector<int>({5,6,7,8,9,1,3,4});
+    s.missingTwo(v);
+    return 0;
+}

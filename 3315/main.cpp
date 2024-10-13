8b5c93d9952c2b8e420fc26e0353a3c515368726
AutJ <autumnal_joy@foxmail.com>
2024-10-13 14:39:11 +0800

    3315. 构造最小位运算数组 II

---
+++ 3315/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+using namespace std;
+
+
+//   0b100111
+// = 0b100011
+//  |0b100100
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int> &nums) {
+
+        auto res = vector<int>();
+
+        for (auto p: nums) {
+            if (p == 2) {
+                res.push_back(-1);
+            } else {
+                int mask =(~p & -~p) >> 1;
+                res.push_back(p ^ mask);
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    auto v = vector<int>({2, 3, 5, 7});
+    s.minBitwiseArray(v);
+    return 0;
+}

8335dc4653a20100c4b0cabd97fd9980ca021b9a
AutJ <autumnal_joy@foxmail.com>
2024-10-27 23:58:04 +0800

    面试题 02.08. 环路检测

---
+++ Interview/Interview02.08/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        auto slow = head, fast = head;
+        int step = 0;
+        do {
+            if (!fast || !fast->next) {
+                return nullptr;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+            ++step;
+        } while (slow != fast);
+        while (head != slow) {
+            head = head->next;
+            slow = slow->next;
+        }
+        return head;
+    }
+};
+
+int main() {
+
+    return 0;
+}

d2e98c3d28d432b955fa087e671237ac2e6fee69
AutJ <autumnal_joy@foxmail.com>
2024-10-28 14:23:15 +0800

    面试题 02.05. 链表求和

---
+++ Interview/Interview02.05/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        auto dummy = ListNode();
+        auto *p = &dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            if (l1) {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            p->next = new ListNode(carry % 10);
+            p = p->next;
+            carry /= 10;
+        }
+        return dummy.next;
+    }
+};
+
+int main() {
+
+    return 0;
+}

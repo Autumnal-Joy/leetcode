4a9f8ad8f8ac039129af5caaf80c0736777194f2
AutJ <autumnal_joy@foxmail.com>
2024-11-01 16:57:06 +0800

    79. 单词搜索: Optim

--- 0001-1000/0079/main.cpp
+++ 0001-1000/0079/main.cpp
@@ -1,47 +1,48 @@
-#include <algorithm>
-#include <iostream>
-#include <numeric>
-#include <string>
-#include <unordered_set>
-#include <vector>
+#ifdef LEETCODE
+#define DEBUG
+#include <bits/stdc++.h>
 
 using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+#endif
 
 class Solution {
+    bool dfs(vector<vector<char>> &board, const string &word,
+             int i, int j, int depth,
+             vector<vector<bool>> &visited) {
+        if (board[i][j] != word[depth]) return false;
+        if (depth == word.size() - 1) return true;
+        auto res = false;
+        visited[i][j] = true;
+        if (i - 1 >= 0 && !visited[i - 1][j]) res = dfs(board, word, i - 1, j, depth + 1, visited);
+        if (i + 1 < board.size() && !visited[i + 1][j]) res = res || dfs(board, word, i + 1, j, depth + 1, visited);
+        if (j - 1 >= 0 && !visited[i][j - 1]) res = res || dfs(board, word, i, j - 1, depth + 1, visited);
+        if (j + 1 < board[0].size() && !visited[i][j + 1]) res = res || dfs(board, word, i, j + 1, depth + 1, visited);
+        visited[i][j] = false;
+        return res;
+    }
+
 public:
-    bool inBoard(const vector<vector<char>> &board, int x, int y) {
-        return x >= 0 && x < board.size() && y >= 0 && y < board[0].size();
-    }
-
-    bool dfs(const vector<vector<char>> &board, const string &word, int depth,
-             int x, int y, vector<vector<bool>> &visited) {
-        if (board[x][y] != word[depth]) {
-            return false;
-        }
-        if (depth + 1 == word.size()) {
-            return true;
-        }
-        visited[x][y] = true;
-        for (int i = -1; i <= 1; ++i) {
-            for (int j = -1; j <= 1; ++j) {
-                if (abs(i) + abs(j) == 1 && inBoard(board, x + i, y + j) &&
-                    !visited[x + i][y + j]) {
-                    if (dfs(board, word, depth + 1, x + i, y + j, visited)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        visited[x][y] = false;
-        return false;
-    }
-
     bool exist(vector<vector<char>> &board, const string &word) {
-        auto visited = vector<vector<bool>>(board.size(),
-                                            vector<bool>(board[0].size(), false));
+        auto visited = vector(board.size(), vector(board[0].size(), false));
         for (int i = 0; i < board.size(); ++i) {
             for (int j = 0; j < board[0].size(); ++j) {
-                if (dfs(board, word, 0, i, j, visited)) {
+                if (dfs(board, word, i, j, 0, visited)) {
                     return true;
                 }
             }
@@ -50,16 +51,9 @@ public:
     }
 };
 
-// ABCE
-// SFES
-// ADEE
-
+#ifdef LEETCODE
 int main() {
-    auto s = Solution();
-    vector<vector<char>> board = {
-            {'A', 'B', 'C', 'E'},
-            {'S', 'F', 'E', 'S'},
-            {'A', 'D', 'E', 'E'}};
-    cout << s.exist(board, "ABCESEEEFS") << endl;
+
     return 0;
 }
+#endif

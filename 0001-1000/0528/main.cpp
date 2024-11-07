9e53fb5e28c1317cdef06aca7a286e40a20c7016
AutJ <autumnal_joy@foxmail.com>
2024-11-07 11:18:52 +0800

    528. 按权重随机选择

---
+++ 0001-1000/0528/main.cpp
@@ -0,0 +1,89 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
+
+class Solution {
+    vector<int> preSum;
+    random_device rd;
+    mt19937 gen;
+    uniform_int_distribution<> dist;
+
+public:
+    explicit Solution(const vector<int> &weight) : gen(rd()), dist(0, 1e9) {
+        std::partial_sum(weight.begin(), weight.end(), std::back_inserter(preSum));
+    }
+
+    int pickIndex() {
+        const auto target = dist(gen) % preSum.back();
+        return std::upper_bound(preSum.begin(), preSum.end(), target) - preSum.begin();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

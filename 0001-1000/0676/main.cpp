a3279e879fb61d23f66748c3e146c58ac9a10ce9
AutJ <autumnal_joy@foxmail.com>
2024-11-07 17:27:16 +0800

    676. 实现一个魔法字典

---
+++ 0001-1000/0676/main.cpp
@@ -0,0 +1,104 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
+
+class MagicDictionary {
+    vector<string> dict;
+
+public:
+    MagicDictionary() = default;
+
+    void buildDict(const vector<string> &dictionary) {
+        dict = dictionary;
+    }
+
+    static bool check(const string &word, const string &target) {
+        if (word.size() != target.size()) return false;
+        auto cnt = 0;
+        for (size_t i = 0; i < word.size(); ++i) {
+            if (word[i] != target[i]) {
+                ++cnt;
+            }
+            if (cnt > 1) {
+                return false;
+            }
+        }
+        return cnt == 1;
+    }
+
+    bool search(const string &searchWord) {
+        return ranges::any_of(dict, [&](const auto &word) {
+            return check(word, searchWord);
+        });
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

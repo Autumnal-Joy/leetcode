611fc9e2586492ab849536872260e7877039fdca
AutJ <autumnal_joy@foxmail.com>
2024-11-08 23:02:04 +0800

    220. 存在重复元素 III: optim

--- 0001-1000/0220/main.cpp
+++ 0001-1000/0220/main.cpp
@@ -1,76 +1,92 @@
-#include <algorithm>
-#include <iostream>
-#include <list>
-#include <vector>
+#ifdef LEETCODE
+#include <bits/stdc++.h>
 
 using namespace std;
+#endif
 
-class Window {
-private:
-    list<int> data;
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
 
-public:
-    int minDiff;
-
-    Window(vector<int> &&nums) : minDiff(INT_MAX) {
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); i++) {
-            minDiff = min({minDiff, abs(nums[i - 1] - nums[i])});
-        }
-        copy(nums.begin(), nums.end(), back_inserter(data));
-    }
-
-    void replace(int replaced, int replacing) {
-        if (replaced == replacing) {
-            return;
-        }
-        auto it = find(data.begin(), data.end(), replaced);
-        data.erase(it);
-        it = data.begin();
-        if (data.empty()) {
-            // 列表为空
-            data.push_back(replacing);
-        } else if (replacing <= *it) {
-            minDiff = min({minDiff, abs(*it - replacing)});
-            data.push_front(replacing);
-        } else if (replacing >= data.back()) {
-            minDiff = min({minDiff, abs(data.back() - replacing)});
-            data.push_back(replacing);
-        } else {
-            while (it != data.end() && next(it) != data.end() &&
-                   *next(it) < replacing) {
-                ++it;
-            }
-            minDiff =
-                    min({minDiff, abs(*it - replacing), abs(*next(it) - replacing)});
-            data.insert(next(it), replacing);
-        }
-    }
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
 };
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
 
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int> &nums, int indexDiff,
-                                       int valueDiff) {
-        int windowSize = min({indexDiff + 1, (int) nums.size()});
-        auto w =
-                Window(std::move(vector<int>(nums.begin(), nums.begin() + windowSize)));
-        if (w.minDiff <= valueDiff) {
-            return true;
-        }
-        for (int i = windowSize; i < nums.size(); i++) {
-            w.replace(nums[i - windowSize], nums[i]);
-            if (w.minDiff <= valueDiff) {
-                return true;
+    bool containsNearbyAlmostDuplicate(const vector<int> &nums, const int indexDiff,
+                                       const int valueDiff) {
+        auto s = multiset<int>();
+        long long minDiff = 0ll + valueDiff + 1;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = s.upper_bound(nums[i]);
+            if (it != s.begin()) {
+                minDiff = std::min({minDiff, 0ll + nums[i] - *std::prev(it)});
+            }
+            if (it != s.end()) {
+                minDiff = std::min({minDiff, 0ll + *it - nums[i]});
+            }
+            if (minDiff <= valueDiff) return true;
+            s.insert(it, nums[i]);
+            if (i >= indexDiff) {
+                s.erase(nums[i - indexDiff]);
             }
         }
         return false;
     }
 };
 
+#ifdef LEETCODE
 int main() {
     Solution s;
-    vector<int> nums = {1, 3, 1};
-    cout << s.containsNearbyAlmostDuplicate(nums, 1, 1) << endl;
+    const auto nums = vector({1, 5, 9, 1, 5, 9});
+    cout << s.containsNearbyAlmostDuplicate(nums, 2, 3) << endl;
     return 0;
 }
+#endif

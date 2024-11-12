1b974c83b5faf1f82adb4fdb5191e9c1c28cef25
AutJ <autumnal_joy@foxmail.com>
2024-11-12 13:14:45 +0800

    515. 在每个树行中找最大值

---
+++ 0001-1000/0515/main.cpp
@@ -0,0 +1,104 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode *root) {
+        if (!root) return {};
+        auto res = vector<int>();
+        auto dq = deque<TreeNode *>();
+        dq.push_back(root);
+        while (!dq.empty()) {
+            auto max = numeric_limits<int>::min();
+            auto n = dq.size();
+            while (n--) {
+                const auto front = dq.front();
+                dq.pop_front();
+                max = std::max({max, front->val});
+                if (front->left) {
+                    dq.push_back(front->left);
+                }
+                if (front->right) {
+                    dq.push_back(front->right);
+                }
+            }
+            res.push_back(max);
+        }
+        return res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

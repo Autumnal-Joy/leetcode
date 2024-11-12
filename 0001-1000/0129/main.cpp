b212fb46353f6235c4ec638cb9eb0a6ee5f2ddfd
AutJ <autumnal_joy@foxmail.com>
2024-11-12 12:51:50 +0800

    129. 求根节点到叶节点数字之和

---
+++ 0001-1000/0129/main.cpp
@@ -0,0 +1,99 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int res = 0;
+    void dfs(TreeNode *root, int sum) {
+        sum = sum * 10 + root->val;
+        if (!root->left && !root->right) {
+            res += sum;
+        }
+        if (root->left) {
+            dfs(root->left, sum);
+        }
+        if (root->right) {
+            dfs(root->right, sum);
+        }
+    }
+
+public:
+    int sumNumbers(TreeNode *root) {
+        dfs(root, 0);
+        return res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

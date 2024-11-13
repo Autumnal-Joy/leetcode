caf08f874b1147b134075592f34dd66f5f24578d
AutJ <autumnal_joy@foxmail.com>
2024-11-13 10:24:53 +0800

    735. 小行星碰撞

---
+++ 0001-1000/0735/main.cpp
@@ -0,0 +1,88 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
+
+class Solution {
+public:
+    vector<int> asteroidCollision(const vector<int> &asteroids) {
+        auto st = deque<int>();
+        for (const int asteroid: asteroids) {
+            if (asteroid > 0) {
+                st.push_back(asteroid);
+            } else {
+                while (!st.empty() && st.back() > 0 && st.back() + asteroid < 0) {
+                    st.pop_back();
+                }
+                if (st.empty() || st.back() < 0) {
+                    st.push_back(asteroid);
+                } else if (!st.empty() && st.back() + asteroid == 0) {
+                    st.pop_back();
+                }
+            }
+        }
+        return {st.begin(), st.end()};
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

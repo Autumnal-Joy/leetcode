04e7105c281d5fa3a5f262ab745beb4a9e272d5b
AutJ <autumnal_joy@foxmail.com>
2024-11-13 14:53:22 +0800

    430. 扁平化多级双向链表

---
+++ 0001-1000/0430/main.cpp
@@ -0,0 +1,102 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
+
+#ifdef LEETCODE
+class Node {
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node *child;
+};
+#endif
+
+class Solution {
+public:
+    Node *flatten(Node *head) {
+        if (!head) return nullptr;
+        auto header = Node();
+        auto pN = &header;
+        auto st = stack<Node *>({head});
+        while (!st.empty()) {
+            const auto top = st.top();
+            st.pop();
+            pN->next = top;
+            top->prev = pN;
+            if (top->next) {
+                st.push(top->next);
+            }
+            if (top->child) {
+                st.push(top->child);
+                top->child = nullptr;
+            }
+            pN = top;
+        }
+        header.next->prev = nullptr;
+        return header.next;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

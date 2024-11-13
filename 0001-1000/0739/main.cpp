d01d7e58ed50ca691ae2d9e911e2e3e585e2ae0e
AutJ <autumnal_joy@foxmail.com>
2024-11-13 09:59:47 +0800

    739. 每日温度: optim

--- 0001-1000/0739/main.cpp
+++ 0001-1000/0739/main.cpp
@@ -1,38 +1,86 @@
-#include <deque>
-#include <iostream>
-#include <vector>
+#ifdef LEETCODE
+#include <bits/stdc++.h>
 
 using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
 
 class Solution {
 public:
     vector<int> dailyTemperatures(const vector<int> &temperatures) {
-        vector<int> res(temperatures.size());
-        deque<int> stack;
+        auto res = vector(temperatures.size(), 0);
+        auto st = stack<int>();
         for (int i = 0; i < temperatures.size(); i++) {
-            if (stack.empty() || temperatures[stack.back()] >= temperatures[i]) {
-                stack.emplace_back(i);
-            } else {
-                while (!stack.empty() && temperatures[stack.back()] < temperatures[i]) {
-                    res[stack.back()] = i - stack.back();
-                    stack.pop_back();
-                }
-                stack.emplace_back(i);
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+                const auto top = st.top();
+                st.pop();
+                res[top] = i - top;
             }
+            st.push(i);
         }
-        while (!stack.empty()) {
-            res[stack.back()] = 0;
-            stack.pop_back();
-        }
-
         return res;
     }
 };
 
+#ifdef LEETCODE
 int main() {
     auto s = Solution();
     auto answer =
             s.dailyTemperatures(vector<int>{73, 74, 75, 71, 69, 72, 76, 73});
-    std::copy(answer.begin(), answer.end(), ostream_iterator<int>(cout, " "));
+    ranges::copy(answer, ostream_iterator<int>(cout, " "));
     return 0;
 }
+#endif

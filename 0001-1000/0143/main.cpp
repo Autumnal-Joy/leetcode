7fb39787362663328b76ac420c5a2a81f333f8a6
AutJ <autumnal_joy@foxmail.com>
2024-11-13 15:04:38 +0800

    143. 重排链表: optim

--- 0001-1000/0143/main.cpp
+++ 0001-1000/0143/main.cpp
@@ -1,6 +1,9 @@
+#ifdef LEETCODE
+#define DEBUG
 #include <bits/stdc++.h>
 
 using namespace std;
+#endif
 
 static auto _ = []() {
     ios::sync_with_stdio(false);
@@ -9,28 +12,71 @@ static auto _ = []() {
     return 0;
 }();
 
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
 
 struct ListNode {
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
 };
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     void reorderList(ListNode *head) {
-        int n = 0;
-        auto temp = head;
-        while (temp) {
-            ++n;
-            temp = temp->next;
-        }
-        if (n <= 2) return;
-        auto mid = head;
-        for (int i = 0; i < n / 2; ++i) {
+        auto header = ListNode(0, head);
+        auto temp = &header, mid = &header;
+        while (temp && temp->next) {
             mid = mid->next;
+            temp = temp->next->next;
         }
         auto st = stack<ListNode *>();
         temp = mid->next;
@@ -40,7 +86,7 @@ public:
             temp = temp->next;
         }
         while (!st.empty()) {
-            auto top = st.top();
+            const auto top = st.top();
             st.pop();
             top->next = head->next;
             head->next = top;
@@ -49,7 +95,9 @@ public:
     }
 };
 
+#ifdef LEETCODE
 int main() {
 
     return 0;
 }
+#endif

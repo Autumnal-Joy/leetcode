65b3d7e7a926567247563a3f009c79c07469d0e5
AutJ <autumnal_joy@foxmail.com>
2024-11-13 10:35:12 +0800

    150. 逆波兰表达式求值

---
+++ 0001-1000/0150/main.cpp
@@ -0,0 +1,90 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        TreeNode *node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+#endif
+
+class Solution {
+public:
+    int evalRPN(const vector<string> &tokens) {
+        auto st = stack<long long>();
+        auto op = unordered_map<string, function<long long(long long, long long)>>();
+        op["+"] = [](const long long a, const long long b) { return a + b; };
+        op["-"] = [](const long long a, const long long b) { return a - b; };
+        op["*"] = [](const long long a, const long long b) { return a * b; };
+        op["/"] = [](const long long a, const long long b) { return a / b; };
+        for (const auto &token: tokens) {
+            if (op.contains(token)) {
+                const long long b = st.top();
+                st.pop();
+                const long long a = st.top();
+                st.pop();
+                st.push(op[token](a, b));
+            } else {
+                st.push(stoll(token));
+            }
+        }
+        return static_cast<int>(st.top());
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

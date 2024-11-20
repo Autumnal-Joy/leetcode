0f3166ccaee8698738cfca4161c7de6bb21dbddc
AutJ <autumnal_joy@foxmail.com>
2024-11-20 15:36:06 +0800

    6. Z 字形变换

---
+++ 0001-1000/0006/main2.cpp
@@ -0,0 +1,117 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+public:
+    string convert(const string &s, const int numRows) {
+        if (numRows == 1) return s;
+        auto circle = vector(numRows, vector(2, 0));
+        for (int i = 0; i < numRows; ++i) {
+            circle[i][0] = 2 * (numRows - i - 1);
+            circle[i][1] = 2 * i;
+        }
+        auto res = string();
+        res.reserve(s.size());
+        for (int i = 0; i < numRows; ++i) {
+            int turn = 0, j = i;
+            while (j < s.size()) {
+                if (circle[i][turn]) {
+                    res.push_back(s[j]);
+                    j += circle[i][turn];
+                }
+                turn = 1 - turn;
+            }
+        }
+        return res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

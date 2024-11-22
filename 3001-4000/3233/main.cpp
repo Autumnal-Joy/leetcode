d393a61725939c0f47333dd75fc0c279d7be1c1c
AutJ <autumnal_joy@foxmail.com>
2024-11-22 10:18:25 +0800

    3233. 统计不是特殊数字的数字数量

---
+++ 3001-4000/3233/main.cpp
@@ -0,0 +1,123 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+#define N 40000
+
+auto isPrime = bitset<N + 1>();
+
+auto init = [] {
+    isPrime.set();
+    isPrime.reset(0);
+    isPrime.reset(1);
+    for (int i = 2; i <= N; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= N; j += i) {
+                isPrime.reset(j);
+            }
+        }
+    }
+    return 0;
+}();
+
+class Solution {
+public:
+    int nonSpecialCount(const int l, const int r) {
+        const int begin = static_cast<int>(std::sqrt(l - 1)) + 1;
+        const int end = static_cast<int>(std::sqrt(r));
+        auto mask = bitset<N + 1>();
+        for (int i = begin; i <= end; ++i) {
+            mask.set(i);
+        }
+        return r - l + 1 - static_cast<int>((mask & isPrime).count());
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    Solution().nonSpecialCount(12345, 54321);
+    return 0;
+}
+#endif

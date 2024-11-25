bf6b0fc2f2c5a06853f1af3943087733d948e2e8
AutJ <autumnal_joy@foxmail.com>
2024-11-25 10:52:14 +0800

    29. 两数相除

---
+++ 0001-1000/0029/main.cpp
@@ -0,0 +1,116 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+public:
+    int divide(int dividend, const int divisor) {
+        if (dividend == numeric_limits<int>::max() && divisor == 1) return numeric_limits<int>::max();
+        if (dividend == numeric_limits<int>::max() && divisor == -1) return -numeric_limits<int>::max();
+        if (dividend == numeric_limits<int>::min() && divisor == 1) return numeric_limits<int>::min();
+        if (dividend == numeric_limits<int>::min() && divisor == -1) return numeric_limits<int>::max();
+        if (dividend > 0) return -divide(-dividend, divisor);
+        if (divisor > 0) return -divide(dividend, -divisor);
+        if (dividend > divisor) return 0;
+        auto res = 0;
+        while (dividend <= divisor) {
+            auto temp = divisor, coe = 1;
+            while (temp > numeric_limits<int>::min() >> 1 && temp + temp > dividend) {
+                temp <<= 1;
+                coe <<= 1;
+            }
+            dividend -= temp;
+            res += coe;
+        }
+        return res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    Solution().divide(-2147483648, 1);
+    return 0;
+}
+#endif

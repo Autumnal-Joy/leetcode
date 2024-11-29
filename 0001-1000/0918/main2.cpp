4492b2915383533a02f65c2a0b8c23c79ee27138
AutJ <autumnal_joy@foxmail.com>
2024-11-29 16:06:32 +0800

    918. 环形子数组的最大和

---
+++ 0001-1000/0918/main2.cpp
@@ -0,0 +1,115 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+public:
+    int maxSubarraySumCircular(const vector<int> &nums) {
+        const auto n = static_cast<int>(nums.size());
+        auto maxPrefix = vector(n, 0ll), maxSuffix = vector(n, 0ll);
+        auto prefix = 0ll, suffix = 0ll, subSum = 0ll, maxSubSum = -0x3f3f3f3fll;
+        for (int i = 0; i < n; ++i) {
+            prefix += nums[i];
+            suffix += nums[n - i - 1];
+            subSum = std::max({subSum, 0ll}) + nums[i];
+
+            maxPrefix[i] = i - 1 >= 0 ? std::max({maxPrefix[i - 1], prefix}) : prefix;
+            maxSuffix[n - i - 1] = n - i < n ? std::max({maxSuffix[n - i], suffix}) : suffix;
+            maxSubSum = std::max({maxSubSum, subSum});
+        }
+        auto res = maxSubSum;
+        for (int i = 1; i <= n - 2; ++i) {
+            res = std::max({res, maxPrefix[i - 1] + maxSuffix[i + 1]});
+        }
+        return static_cast<int>(res);
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

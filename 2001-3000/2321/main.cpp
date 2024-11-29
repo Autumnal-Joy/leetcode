fb1b12abdd25e3ddb72aa3abd9509a429739f5f8
AutJ <autumnal_joy@foxmail.com>
2024-11-29 16:30:12 +0800

    2321. 拼接数组的最大分数

---
+++ 2001-3000/2321/main.cpp
@@ -0,0 +1,115 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+    static int solve(const vector<int> &nums1, const vector<int> &nums2) {
+        const auto n = static_cast<int>(nums1.size());
+        auto nums = nums2;
+        auto sum1 = 0;
+        for (int i = 0; i < n; ++i) {
+            nums[i] -= nums1[i];
+            sum1 += nums1[i];
+        }
+        auto dp = 0, maxSubSum = 0;
+        for (int i = 0; i < n; ++i) {
+            dp = std::max(dp, 0) + nums[i];
+            maxSubSum = std::max(maxSubSum, dp);
+        }
+        return sum1 + maxSubSum;
+    }
+
+public:
+    int maximumsSplicedArray(const vector<int> &nums1, const vector<int> &nums2) {
+        return std::max(solve(nums1, nums2), solve(nums2, nums1));
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

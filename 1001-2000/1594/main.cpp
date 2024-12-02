bf3ad80d51a85f8a862fbc46c36ff67b7379762e
AutJ <autumnal_joy@foxmail.com>
2024-12-02 14:21:37 +0800

    1594. 矩阵的最大非负积

---
+++ 1001-2000/1594/main.cpp
@@ -0,0 +1,130 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+    long long M = 1e9 + 7;
+
+public:
+    int maxProductPath(const vector<vector<int>> &grid) {
+        const auto m = static_cast<int>(grid.size());
+        const auto n = static_cast<int>(grid[0].size());
+        auto min_dp = vector(m, vector(n, 0ll));
+        auto max_dp = vector(m, vector(n, 0ll));
+        min_dp[0][0] = max_dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; ++i) {
+            min_dp[i][0] = max_dp[i][0] = min_dp[i - 1][0] * grid[i][0];
+        }
+        for (int j = 1; j < n; ++j) {
+            min_dp[0][j] = max_dp[0][j] = min_dp[0][j - 1] * grid[0][j];
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                max_dp[i][j] = std::max(
+                        max_dp[i - 1][j] * grid[i][j],
+                        std::max(
+                                min_dp[i - 1][j] * grid[i][j],
+                                std::max(
+                                        max_dp[i][j - 1] * grid[i][j],
+                                        min_dp[i][j - 1] * grid[i][j])));
+                min_dp[i][j] = std::min(
+                        max_dp[i - 1][j] * grid[i][j],
+                        std::min(
+                                min_dp[i - 1][j] * grid[i][j],
+                                std::min(
+                                        max_dp[i][j - 1] * grid[i][j],
+                                        min_dp[i][j - 1] * grid[i][j])));
+            }
+        }
+        return max_dp[m - 1][n - 1] < 0 ? -1 : static_cast<int>(max_dp[m - 1][n - 1] % M);
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

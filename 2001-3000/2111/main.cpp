22be9b4d04f43df989b627da4da13bbc2f0c2358
AutJ <autumnal_joy@foxmail.com>
2024-12-07 18:34:08 +0800

    2111. 使数组 K 递增的最少操作次数

---
+++ 2001-3000/2111/main.cpp
@@ -0,0 +1,120 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+    static int solve(const vector<int> &arr, const int begin, const int k) {
+        auto dp = vector<int>();
+        auto n = 0;
+        for (int i = begin; i < arr.size(); i += k, ++n) {
+            const auto it = ranges::upper_bound(dp, arr[i]);
+            if (it == dp.end()) {
+                dp.push_back(arr[i]);
+            } else {
+                *it = arr[i];
+            }
+        }
+        return n - static_cast<int>(dp.size());
+    }
+
+public:
+    int kIncreasing(const vector<int> &arr, const int k) {
+        const auto n = static_cast<int>(arr.size());
+        if (k == n) return 0;
+        auto res = 0;
+        for (int i = 0; i < k; ++i) {
+            res += solve(arr, i, k);
+        }
+        return res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    const auto arr = vector({4, 1, 5, 2, 6, 2});
+    Solution().kIncreasing(arr, 2);
+    return 0;
+}
+#endif

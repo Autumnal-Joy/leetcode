ff90eae3c8b2299b629fccefcd2123d1446e63a3
AutJ <autumnal_joy@foxmail.com>
2024-12-07 14:17:02 +0800

    1671. 得到山形数组的最少删除次数

---
+++ 1001-2000/1671/main.cpp
@@ -0,0 +1,126 @@
+#ifdef LEETCODE
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int> &nums) {
+        const auto n = static_cast<int>(nums.size());
+        auto dp1 = vector(n, 0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp1[i] = std::max(dp1[i], dp1[j]);
+                }
+            }
+            ++dp1[i];
+        }
+        ranges::reverse(nums);
+        auto dp2 = vector(n, 0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp2[i] = std::max(dp2[i], dp2[j]);
+                }
+            }
+            ++dp2[i];
+        }
+        auto res = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            if (dp1[i] > 1 && dp2[n - 1 - i] > 1) {
+                res = std::max(res, dp1[i] + dp2[n - 1 - i] - 1);
+            }
+        }
+        return n - res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    auto nums = vector({100, 92, 89, 77, 74, 66, 64, 66, 64});
+    Solution().minimumMountainRemovals(nums);
+    return 0;
+}
+#endif

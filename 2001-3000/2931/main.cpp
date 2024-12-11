3788fd16b28ed56b2ba588b5e2e665b083480f83
AutJ <autumnal_joy@foxmail.com>
2024-12-12 00:57:08 +0800

    2931. 购买物品的最大开销

---
+++ 2001-3000/2931/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+    using PLI = pair<long long, int>;
+
+public:
+    long long maxSpending(const vector<vector<int>> &values) {
+        const auto m = static_cast<int>(values.size());
+        const auto n = static_cast<int>(values[0].size());
+        auto pq = priority_queue<PLI, vector<PLI>, std::greater<>>();
+        for (int i = 0; i < m; ++i) {
+            pq.emplace(values[i][n - 1], i);
+        }
+        auto idx = vector(m, n - 2);
+        auto res = 0ll;
+        for (int i = 1; i <= m * n; ++i) {
+            const auto [val, id] = pq.top();
+            pq.pop();
+            res += val * i;
+            if (idx[id] >= 0) {
+                pq.emplace(values[id][idx[id]--], id);
+            }
+        }
+        return res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    auto values = vector<vector<int>>({{8, 5, 2}, {6, 4, 1}, {9, 7, 3}});
+    Solution().maxSpending(values);
+    return 0;
+}
+#endif

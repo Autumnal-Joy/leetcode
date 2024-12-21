4fe22b34f16430b71fcc51443d5efa1b73def55a
AutJ <autumnal_joy@foxmail.com>
2024-12-22 01:28:19 +0800

    1387. 将整数按权重排序

---
+++ 1001-2000/1387/main.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+auto dp = unordered_map<int, int>();
+
+auto _2 = [] {
+    function<int(int)> dfs = [&](const int x) -> int {
+        if (x != 1 && dp[x] == 0) {
+            dp[x] = x & 1 ? dfs(3 * x + 1) + 1 : dfs(x / 2) + 1;
+        }
+        return dp[x];
+    };
+    for (int i = 1; i <= 1000; ++i) {
+        dp[i] = dfs(i);
+    }
+    return 0;
+}();
+
+
+class Solution {
+
+public:
+    int getKth(const int lo, const int hi, const int k) {
+        auto id = vector<int>(hi - lo + 1);
+        std::iota(id.begin(), id.end(), 0);
+        ranges::sort(id, [&](auto a, auto b) {
+            return dp[a + lo] == dp[b + lo] ? a < b : dp[a + lo] < dp[b + lo];
+        });
+        return id[k - 1] + lo;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+
+    return 0;
+}
+#endif

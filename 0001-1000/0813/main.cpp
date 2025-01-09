3125c8bf4022cf39e600d9d12b3081281b9b6350
AutJ <autumnal_joy@foxmail.com>
2025-01-09 17:32:46 +0800

    813. 最大平均值和的分组

---
+++ 0001-1000/0813/main.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        const auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+class Solution {
+public:
+    double largestSumOfAverages(const vector<int> &nums, const int k) {
+        const auto n = static_cast<int>(nums.size());
+        auto dp = vector(k + 1, vector(n + 1, 0.0));
+        auto s = 0.0;
+        for (int j = 1; j <= n; ++j) {
+            s += nums[j - 1];
+            dp[1][j] = s / j;
+        }
+        for (int i = 2; i <= k; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                auto sum = 0.0;
+                for (int j2 = j - 1; j2 >= 0; --j2) {
+                    sum += nums[j2];
+                    dp[i][j] = std::max(dp[i][j], dp[i - 1][j2] + sum / (j - j2));
+                }
+            }
+        }
+        return dp[k][n];
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    const auto nums = vector({1, 2, 3, 4, 5, 6, 7});
+    Solution().largestSumOfAverages(nums, 4);
+    return 0;
+}
+#endif

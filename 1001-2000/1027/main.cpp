7721152a28875c174270a34c79b4c4ec2bed6855
AutJ <autumnal_joy@foxmail.com>
2025-01-22 10:10:06 +0800

    1027. 最长等差数列

---
+++ 1001-2000/1027/main.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        const auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+#define Array1(type, size1) array<type, size1>
+#define Array2(type, size1, size2) array<array<type, size2>, size1>
+#define Array3(type, size1, size2, size3) array<array<array<type, size3>, size2>, size1>
+#define MAX_I 0x3f3f3f3f
+#define MIN_I (-MAX_I)
+#define MAX_LL 0x3f3f3f3f3f3f3f3f
+#define MIN_LL (-MAX_LL)
+
+class Solution {
+public:
+    int longestArithSeqLength(const vector<int> &nums) {
+        const auto n = static_cast<int>(nums.size());
+        auto res = 0;
+        for (int diff = -500; diff <= 500; ++diff) {
+            auto dp = vector(501, 0);
+            for (int i = 0; i < n; ++i) {
+                dp[nums[i]] = (0 <= nums[i] - diff && nums[i] - diff <= 500 ? dp[nums[i] - diff] : 0) + 1;
+                res = std::max(res, dp[nums[i]]);
+            }
+        }
+        return res;
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    const auto nums = vector({9, 4, 7, 2, 10});
+    Solution().longestArithSeqLength(nums);
+    return 0;
+}
+#endif

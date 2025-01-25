6df0489ff5ea1c07621c52a05c94c5950528e288
AutJ <autumnal_joy@foxmail.com>
2025-01-25 21:36:12 +0800

    2412. 完成所有交易的初始最少钱数

---
+++ 2001-3000/2412/main.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+#ifdef LEETCODE
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(const int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode *vec2tree(const vector<int> &vec) {
+    if (vec.empty()) {
+        return nullptr;
+    }
+    auto *root = new TreeNode(vec[0]);
+    auto q = queue<TreeNode *>();
+    q.push(root);
+    for (size_t i = 1; i < vec.size(); i += 2) {
+        const auto node = q.front();
+        q.pop();
+        if (vec[i] != -1) {
+            node->left = new TreeNode(vec[i]);
+            q.push(node->left);
+        }
+        if (i + 1 < vec.size() && vec[i + 1] != -1) {
+            node->right = new TreeNode(vec[i + 1]);
+            q.push(node->right);
+        }
+    }
+    return root;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(const int x) : val(x), next(nullptr) {}
+    ListNode(const int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *vec2list(const vector<int> &vec) {
+    ListNode head;
+    ListNode *p = &head;
+    for (const auto &val: vec) {
+        p->next = new ListNode(val);
+        p = p->next;
+    }
+    return head.next;
+}
+vector<vector<int>> read(const string &filename) {
+    auto res = vector<vector<int>>();
+    auto ifs = ifstream(filename);
+    auto line = string();
+    while (getline(ifs, line)) {
+        auto vec = vector<int>();
+        auto iss = istringstream(line);
+        for (int val; iss >> val;) {
+            vec.push_back(val);
+        }
+        res.push_back(std::move(vec));
+    }
+    return res;
+}
+void write(const vector<int> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    ranges::copy(data, ostream_iterator<int>(ofs, " "));
+    ofs << '\n';
+}
+void write(const vector<vector<int>> &data, const string &filename) {
+    auto ofs = ofstream(filename);
+    for (const auto &line: data) {
+        ranges::copy(line, ostream_iterator<int>(ofs, " "));
+        ofs << '\n';
+    }
+}
+#endif
+
+template<typename T, size_t N>
+auto Array() {
+    return array<T, N>({});
+}
+template<typename T, size_t N1, size_t N2>
+auto Array() {
+    return array<array<T, N2>, N1>({});
+}
+template<typename T, size_t N1, size_t N2, size_t N3>
+auto Array() {
+    return array<array<array<T, N3>, N2>, N1>({});
+}
+#define MAX_I 0x3f3f3f3f
+#define MIN_I (-MAX_I)
+#define MAX_LL 0x3f3f3f3f3f3f3f3f
+#define MIN_LL (-MAX_LL)
+
+template<typename Pred>
+long long first(long long l, long long r, Pred pred) {
+    while (l + 1 < r) {
+        const auto m = l + (r - l) / 2;
+        if (pred(m)) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    return r;
+}
+
+class Solution {
+
+public:
+    long long minimumMoney(vector<vector<int>> &transactions) {
+        ranges::sort(transactions, [&](const auto &l, const auto &r) {
+            const auto balance1 = l[1] - l[0];
+            const auto balance2 = r[1] - r[0];
+            if (balance1 < 0 && balance2 < 0) {
+                return r[0] - balance1 > l[0] - balance2;
+            } else if (balance1 < 0 && balance2 >= 0) {
+                return true;
+            } else if (balance1 >= 0 && balance2 < 0) {
+                return false;
+            } else {
+                return l[0] > r[0];
+            }
+        });
+        const auto pred = [&](long long money) {
+            for (const auto &trans: transactions) {
+                if (money < trans[0]) return false;
+                money = money - trans[0] + trans[1];
+            }
+            return true;
+        };
+        return first(-1, 100'000'000'000'001ll, pred);
+    }
+};
+
+#ifdef LEETCODE
+int main() {
+    auto trans = vector<vector<int>>({{2, 1}, {5, 0}, {4, 2}});
+    Solution().minimumMoney(trans);
+    return 0;
+}
+#endif
